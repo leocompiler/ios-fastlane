cd06bdccf1e94901d40fcd036228a040a5c0ed3f
Leonardo <leonardo.silveira@linker.com.br>
2023-07-17 15:34:30 -0300

    Initial Commit

---
+++ ios-fastlane/ios_fastlaneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_fastlaneApp.swift
+//  ios-fastlane
+//
+//  Created by LEONARDO A SILVEIRA on 17/07/23.
+//
+
+import SwiftUI
+
+@main
+struct ios_fastlaneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
